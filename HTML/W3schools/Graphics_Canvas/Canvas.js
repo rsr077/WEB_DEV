4b2ca9876ffc85990f294b5572241f1f9b37c3b2
rsr077 <yourfriend77777777@gmail.com>
2024-06-02 15:30:29 +0530

    new

---
+++ HTML/W3schools/Graphics_Canvas/Canvas.js
@@ -0,0 +1,10 @@
+var canvas = document.querySelector('canvas')
+
+canvas.width =window.innerWidth;
+canvas.hight =window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.fillRact(100, 100, 100);
+c.fillRact(400, 100,100);
+c.fillRact(300,  300, 100);
+console.log(canvas);
